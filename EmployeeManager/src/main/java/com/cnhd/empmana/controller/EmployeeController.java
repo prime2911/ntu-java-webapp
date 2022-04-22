3d2a168bd837e9354ea61b35b602e7f58501ecf1
prime2911 <90593390+prime2911@users.noreply.github.com>
2022-04-22 11:15:49 +0700

    Implemented "Add employee" feature

--- EmployeeManager/src/main/java/com/cnhd/empmana/controller/EmployeeController.java
+++ EmployeeManager/src/main/java/com/cnhd/empmana/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cnhd.empmana.model.Employee;
 import com.cnhd.empmana.services.EmployeeService;
 
 @Controller
@@ -14,8 +17,21 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 	
 	@GetMapping("/")
-	String viewHomePage(Model model) {
+	public String viewHomePage(Model model) {
 		model.addAttribute("empList", employeeService.getAllEmployees());
 		return "index";
+	}
+	
+	@GetMapping("/addEmployee")
+	public String addEmployee(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "newEmployee";
+	}
+	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.saveEmployee(employee);
+		return "redirect:/";
 	}
 }
